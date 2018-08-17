7312156eac36ddf1f28228df6f2e22d8a4cb1077
Slavi <slavi_14@abv.bg>
2018-08-17 11:56:02 +0300

    commit 3

---
+++ app/src/main/java/com/example/stan/androidproject4/BulgarianBordercountriesActivity.java
@@ -0,0 +1,13 @@
+package com.example.stan.androidproject4;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BulgarianBordercountriesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bulgarian_bordercountries);
+    }
+}
